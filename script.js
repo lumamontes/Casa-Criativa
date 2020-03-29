0b8b2bfc6f5b18e8cdf5a968c1301b98bf6b59a3
Luma <lumagoesmontes@gmail.com>
2020-03-29 15:46:31 -0300

    Front-end OK

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ document
 .querySelector("#modal")
 .classList
 .toggle("hide")
+
+document
+    .querySelector("body")
+    .classList
+    .toggle("hideScroll")
+
+
+  document
+  .querySelector("#modal")
+  .classList
+  .toggle("addScroll")  
 }
 
 
